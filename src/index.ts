dd8524381ba8588ea7a51ff9e1936d221ba37fec
Rami Saarivuori <37345715+Appelsiini1@users.noreply.github.com>
2025-08-01 01:36:16 +0300

    Add HTML and static express serving

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-const PORT = 9000;
-
 import express from "express";
+import path from "node:path";
 
+const PORT = 9000;
 const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res) => {
+  res.status(404);
+  res.send("<h1>Error 404: Resource not found.</h1>");
+});
+
 app.listen(PORT, () =>
   console.log(`Server online at http://localhost:${PORT}`)
 );
 
-app.get("/image", (req, res) => {});
+//app.get("/image", (req, res) => {});
 
 app.post("/database", (req, res) => {});
