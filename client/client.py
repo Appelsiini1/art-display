dc6349378a60d558aa7be26bcc815fdf5c102ae9
Rami Saarivuori <37345715+Appelsiini1@users.noreply.github.com>
2025-09-18 01:50:39 +0300

    Format client files

--- client/client.py
+++ client/client.py
@@ -1,12 +1,19 @@
 from sys import exit
-from client_lib import add_file, update_file, metadata_value, get_metadata_value, get_display_value
+from client_lib import (
+    add_file,
+    update_file,
+    metadata_value,
+    get_metadata_value,
+    get_display_value,
+)
 from dotenv import load_dotenv
+
 
 def menu():
     print("Select option:")
     print("1) Add file to database")
     print("2) Modify file in database")
-    print("3) Delete file from database") # not implemented on server yet
+    print("3) Delete file from database")  # not implemented on server yet
     print("4) Add/Update settings value")
     print("5) Get settings value")
     print("6) Get display file metadata")
@@ -19,6 +26,7 @@ def menu():
         print("Selection was not a number. Please input your selection as an integer.")
     return user_in
 
+
 def main():
 
     selection = -1
@@ -28,28 +36,29 @@ def main():
     while selection != 0:
         selection = menu()
 
-        if (selection == 1):
+        if selection == 1:
             back_dir = add_file(init_dir)
             if back_dir:
                 init_dir = back_dir
-        elif (selection == 2):
+        elif selection == 2:
             img_id = input("Image ID to update: ")
             update_file(img_id)
-        elif (selection == 3):
+        elif selection == 3:
             print("Not implemented.")
-        elif (selection == 4):
+        elif selection == 4:
             metadata_value()
-        elif (selection == 5):
+        elif selection == 5:
             get_metadata_value()
-        elif (selection == 6):
+        elif selection == 6:
             get_display_value()
-        elif (selection == 0):
+        elif selection == 0:
             print("Exiting...")
             exit(0)
         else:
             print("Unknown selection.")
         print()
 
-if __name__ == "__main__" :
+
+if __name__ == "__main__":
     load_dotenv(verbose=True)
     main()
--- client/client_lib.py
+++ client/client_lib.py
@@ -5,41 +5,45 @@ from pprint import pprint
 
 FILETYPES = (("Image files", [".png", ".jpg", ".jpeg", ".gif"]),)
 
+
 def get_api():
     api = getenv("IMG_API_URL")
     if api == None:
         raise ValueError("No server api URL defined.")
     return api
 
-def replace_win_path(path:str):
+
+def replace_win_path(path: str):
     return path.replace("R:/", "")
 
 
-def add_file(inital_dir:str):
+def add_file(inital_dir: str):
     api = get_api()
-    
+
     root = Tk()
-    root.wm_attributes('-topmost', 1)
+    root.wm_attributes("-topmost", 1)
     root.withdraw()
-    file_selections = filedialog.askopenfilenames(initialdir=inital_dir, title="Select file(s) to add", filetypes=FILETYPES)
+    file_selections = filedialog.askopenfilenames(
+        initialdir=inital_dir, title="Select file(s) to add", filetypes=FILETYPES
+    )
     root.destroy()
     if not file_selections:
         print("No files selected or operation canceled.")
         return
-    
-#   artist: string;
-#   path: string;
-#   type: string;
-#   rating: "sfw" | "nsfw";
+
+    #   artist: string;
+    #   path: string;
+    #   type: string;
+    #   rating: "sfw" | "nsfw";
     artist = input("Artist name: ")
-    
+
     while True:
         rating = input("Rating (sfw | nsfw): ").lower()
         if rating not in ["sfw", "nsfw"]:
             print("Only 'sfw' or 'nsfw' value allowed.")
         else:
             break
-    
+
     type_in = input("Image type: ")
 
     try:
@@ -48,12 +52,14 @@ def add_file(inital_dir:str):
                 "artist": artist,
                 "path": replace_win_path(file.strip()),
                 "type": type_in,
-                "rating": rating
+                "rating": rating,
             }
-            
-            response = requests.post(api+"/database/add", json=metadata, timeout=5)
+
+            response = requests.post(api + "/database/add", json=metadata, timeout=5)
             if response.status_code is not requests.codes.ok:
-                print(f"Server responded with HTTP {response.status_code}. '{response.reason}', '{response.text}'")
+                print(
+                    f"Server responded with HTTP {response.status_code}. '{response.reason}', '{response.text}'"
+                )
                 break
             else:
                 print(f"OK - {path.basename(replace_win_path(file))}")
@@ -61,14 +67,15 @@ def add_file(inital_dir:str):
         print("Connection error occured: ", e)
         return
     return path.dirname(file_selections[0])
-  
-def update_file(img_id:str):
+
+
+def update_file(img_id: str):
     api = get_api()
-    
+
     try:
-        response = requests.get(api+"/img", {"id":img_id}, timeout=5)
+        response = requests.get(api + "/img", {"id": img_id}, timeout=5)
         response.raise_for_status()
-        
+
     except (ConnectionError, requests.HTTPError) as e:
         print("Connection error occured: ", e)
         return
@@ -86,17 +93,26 @@ def update_file(img_id:str):
         else:
             break
 
-    
     metadata = {
-        "id" : img_id,
-        "artist": artist if (artist != resp["artist"]) and artist != ""  else resp["artist"],
-        "path": replace_win_path(path) if path != resp["path"] and path != "" else resp["path"],
-        "type": type_img if type_img != resp["type"] and type_img != "" else resp["type"],
-        "rating": rating if rating != resp["rating"] and rating != "" else resp["rating"],
+        "id": img_id,
+        "artist": (
+            artist if (artist != resp["artist"]) and artist != "" else resp["artist"]
+        ),
+        "path": (
+            replace_win_path(path)
+            if path != resp["path"] and path != ""
+            else resp["path"]
+        ),
+        "type": (
+            type_img if type_img != resp["type"] and type_img != "" else resp["type"]
+        ),
+        "rating": (
+            rating if rating != resp["rating"] and rating != "" else resp["rating"]
+        ),
     }
 
     try:
-        response = requests.post(api+"/database/update", json=metadata, timeout=5)
+        response = requests.post(api + "/database/update", json=metadata, timeout=5)
         response.raise_for_status()
         print("File updated.")
     except (ConnectionError, requests.HTTPError) as e:
@@ -106,25 +122,28 @@ def update_file(img_id:str):
 
 def metadata_value():
     api = get_api()
-    
+
     name = input("Key: ")
     value = input("Value: ")
 
     try:
-        response = requests.post(api+"/metadata", {"id":name, "value": value}, timeout=5)
+        response = requests.post(
+            api + "/metadata", {"id": name, "value": value}, timeout=5
+        )
         response.raise_for_status()
     except (ConnectionError, requests.HTTPError) as e:
         print("Connection error occured: ", e)
         return
     print("Value added/updated.")
 
+
 def get_metadata_value():
     api = get_api()
-    
+
     name = input("Key: ")
 
     try:
-        response = requests.get(api+"/metadata/get", {"name":name}, timeout=5)
+        response = requests.get(api + "/metadata/get", {"name": name}, timeout=5)
         response.raise_for_status()
         rp_json = response.json()
         print(f"ID: {rp_json["id"]}, Value: {rp_json["value"]}")
@@ -133,13 +152,14 @@ def get_metadata_value():
         return
     print("Value added/updated.")
 
+
 def get_display_value():
     api = get_api()
 
     ID = input("File ID: ")
 
     try:
-        response = requests.get(api+"/img", {"id":ID}, timeout=5)
+        response = requests.get(api + "/img", {"id": ID}, timeout=5)
         response.raise_for_status()
         rp_json = response.json()
         pprint(rp_json)
